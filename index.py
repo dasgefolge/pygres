808f90b98ab6fef278892ddbe628cb97624ccc95
Max Dominik Weber <fenhl@fenhl.net>
2013-05-17 09:11:47 +0000

    Update index.py with a cleaner description

--- index.py
+++ index.py
@@ -8,11 +8,9 @@ def htmain():
         ht.tag("html", content=ht.greshead() +
             ht.tag("body", attributes={"class": "center"}, content=
                 ht.grestoolbar() +
-                ht.tag("h1", content=
-                    ht.tag("a", attributes={"href": "http://fenhl.net/gres/"}, content="Progress") +
-                    " test implementation"
-                ) +
-                ht.tag("p", content="Coming soon!")
+                ht.tag("h1", content="Welcome to Progress") +
+                (ht.tag("p", content="Please log in. Anonymous access is in the works.") if ht.oid() == None else ht.tag("p", content="Nothing to see here. The ability to create tasks is in the works.")) +
+                ht.tag("p", content=ht.tag("a", attributes={"href": "http://fenhl.net/gres/"}, content="What is Progress?"))
             )
         )
     )
