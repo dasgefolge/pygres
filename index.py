f27ff9f9cd410c724051dd68c6d020550d4c7d7f
Max Dominik Weber <fenhl@fenhl.net>
2013-05-21 01:04:13 +0000

    Fix index.py syntax error

--- index.py
+++ index.py
@@ -10,7 +10,7 @@ def htmain():
                 ht.grestoolbar() +
                 ht.tag("a", attributes={"href": "https://github.com/fidera/pygres"}, content=
                     ht.tag("img", attributes={"style": "position: absolute; top: 0; right: 0; border: 0;", "src": "https://s3.amazonaws.com/github/ribbons/forkme_right_green_007200.png", "alt": "Fork me on GitHub"})
-                )
+                ) +
                 ht.tag("h1", content="Welcome to Progress") +
                 (ht.tag("p", content="Please log in. Anonymous access is in the works.") if ht.oid() == None else ht.tag("p", content="Nothing to see here. The ability to create tasks is in the works.")) +
                 ht.tag("p", content=ht.tag("a", attributes={"href": "http://fenhl.net/gres/"}, content="What is Progress?"))
