bd5f8683e19e54c4d45a76aa91beb780973deea3
Max Dominik Weber <fenhl@fenhl.net>
2013-05-30 08:57:42 +0000

    Update forkme to use CSS class

--- index.py
+++ index.py
@@ -4,9 +4,9 @@ import sys
 
 def htmain():
     print(ht.grespage(attributes={"class": "center"}, content=
-        ht.tag("div", attributes={"style": "position: relative;"}, content=
+        ht.tag("div", attributes={"class": "forkme"}, content=
             ht.tag("a", attributes={"href": "https://github.com/fidera/pygres"}, content=
-                ht.tag("img", attributes={"style": "position: absolute; top: 0; right: 0; border: 0;", "src": "https://s3.amazonaws.com/github/ribbons/forkme_right_green_007200.png", "alt": "Fork me on GitHub"})
+                ht.tag("img", attributes={"src": "https://s3.amazonaws.com/github/ribbons/forkme_right_green_007200.png", "alt": "Fork me on GitHub"})
             ) +
             ht.tag("h1", content="Welcome to Progress") +
             (ht.tag("p", content="Please log in. Anonymous access is in the works.") if ht.oid() == None else ht.tag("p", content="Nothing to see here. The ability to create tasks is in the works.")) +
